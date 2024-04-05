58d4a58e85926e4fea9386ee2f7def0235fa7d66
Bamidele Odunlami <bamideleodunlami@bamideles-mbp.home>
2024-04-06 00:34:26 +0100

    investor payout

--- util/task.util.js
+++ util/task.util.js
@@ -18,7 +18,7 @@ async function readInvestorDate (){
     for(let i=0; i<investorDate.length; i++){
         for(let j = 0; j<investorDate[i].investment.length; j++){ //check if one investor has more than one investment
             if (today == investorDate[i].investment[j].payOutDay){
-                mailer.sendPayOutReminder("odunlamibamidelejohn@gmail.com", "bamidele@wosiwosi.co.uk", today, investorDate[i].profile.fname )
+                mailer.sendPayOutReminder("odunlamibamidelejohn@gmail.com", "bamidele@wosiwosi.co.uk", today, investorDate[i].profile.fname );
             }
             
         }
