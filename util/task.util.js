b1637f18a3d76bbc1b88464e6e77b71b83bd4fca
Bamidele Odunlami <bamideleodunlami@bamideles-mbp.home>
2024-04-06 00:27:08 +0100

    payout reminder

--- util/task.util.js
+++ util/task.util.js
@@ -5,21 +5,35 @@ const path = require("path");
 const rootPath = path.resolve(process.cwd());
 appRoot.setPath(rootPath);
 
+const date = new Date()
+
+let today= date.getDate()
+
 const investorDB = require (appRoot + "/model/user.model.js").User
 
 const mailer = require(appRoot + "/util/mailer.util.js");
 
 async function readInvestorDate (){
     const investorDate = await investorDB.find({role:"investor"})
-    for(let i=0; i<investorDB.length-1; i++){
-        console.log(investorDate[i].investment[0].startDate)
+    for(let i=0; i<investorDate.length; i++){
+        for(let j = 0; j<investorDate[i].investment.length; j++){ //check if one investor has more than one investment
+            if (today == investorDate[i].investment[j].payOutDay){
+                mailer.sendPayOutReminder("odunlamibamidelejohn@gmail.com", "bamidele@wosiwosi.co.uk", today, investorDate[i].profile.fname )
+            }
+            
+        }
     }
 }
 
-// readInvestorDate()
-
-cron.schedule('*/1  * * * *', () => {
-    // readInvestorDate()
+// Cron runs every day at 00:00
+cron.schedule('0 0 * * *', () => {
+    readInvestorDate()
+},{
+    scheduled: true,
+    timezone: "Europe/London"
 });
+
+
+
 
 module.exports = cron
